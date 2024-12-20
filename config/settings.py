811f7a4fb57bec43df8a65f34566cb64fc056904
David Akinola <dakinola54@gmail.com>
2024-12-20 22:30:50 +0100

    Updates for heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9)(%7be$d2@)bb38=zn3#oml)xnp^7^8+m^x!jw!#!(kd3**#-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
