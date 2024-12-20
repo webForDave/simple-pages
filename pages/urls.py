f1b416262fbcc0f4cab1a8333fe5bb53ddc1d134
David Akinola <dakinola54@gmail.com>
2024-12-20 22:01:51 +0100

    initial commit

---
+++ pages/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import HomepageView, AboutPageView
+
+urlpatterns = [
+    path('', HomepageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about')
+]
